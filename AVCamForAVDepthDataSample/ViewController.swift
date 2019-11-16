31c06ded1d0ea1126c9df1e0f815c01b57c1cc99
omatty <9mm.cool@gmail.com>
2019-11-17 04:05:04 +0900

    Initial Commit

---
+++ AVCamForAVDepthDataSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVCamForAVDepthDataSample
+//
+//  Created by omatty198 on 2019/11/17.
+//  Copyright © 2019 omatty198. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
